d9529f0a7d3a639ec4caf7cc0c78dc5b3e3f7ad9
Lingfei-Zhang <Javagoshell@gmail.com>
2015-07-24 22:29:37 +0800

    add mafentwo

--- catchutil/Catch.py
+++ catchutil/Catch.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import feedparser
+from bs4 import BeautifulSoup
 
 from DB import *
 import datetime
@@ -13,18 +14,42 @@ import json
 
 
 def catch_autohome():
-    for pageIndex in range(1, 5):
-        bbsPrefix = "http://club.autohome.com.cn/bbs/thread-"
+    bbsPrefix = "http://club.autohome.com.cn/bbs/thread-"
+    for pageIndex in range(1, 6)[::-1]:
         url = "http://club.autohome.com.cn/ajax/essentialtopics?pageindex=" + str(pageIndex) + "&seriesids=&pagesize=35"
         http = urllib.urlopen(url)
         content = unicode(http.read(), 'GBK')
         obj = json.loads(content)
-        for article in obj["data"]:
+        articles = obj["data"]
+        articles.reverse()
+        for article in articles:
             a = bbsPrefix + article["tBBS"] + "-" + str(article["tBBSId"]) + "-" + str(article["topicId"]) + "-1.html"
             if not TRssItemDao.queryTRssItemByLink(a, 3):
                 t_rss_item = TRssItem(title=article["tTitle"], link=a, published_date=datetime.datetime.today(),
-                                      rss_id=3, created_date=str(datetime.datetime.today()))
+                                      rss_id=3, created_date=datetime.datetime.today())
                 TRssItemDao.saveTRssItem(t_rss_item)
+
+"""
+抓取马蜂窝最新游记
+"""
+
+def catch_mafengwo():
+    urlPrefix = "http://www.mafengwo.cn/ajax/ajax_article.php?type=1&start="
+    for pageIndex in range(1, 6)[::-1]:
+        url = urlPrefix + str(pageIndex)
+        http = urllib.urlopen(url)
+        content = http.read()
+        soup = BeautifulSoup(content, "lxml")
+        links = soup.select("div dl dt > a")
+        links.reverse()
+        for a in links:
+            tempArray = a["href"].split("\\")
+            temp_link = "http://www.mafengwo.cn" + tempArray[2] + tempArray[3]
+            if not TRssItemDao.queryTRssItemByLink(temp_link, 4):
+                t_rss_item = TRssItem(title=a.text.split("\\r\\n")[0].decode("unicode_escape").strip(), link=temp_link, published_date=datetime.datetime.today(),
+                                      rss_id=4, created_date=datetime.datetime.today())
+                TRssItemDao.saveTRssItem(t_rss_item)
+        http.close()
 
 
 if __name__ == "__main__":
@@ -36,9 +61,10 @@ if __name__ == "__main__":
             for entry in entries:
                 if not TRssItemDao.queryTRssItemByLink(entry["link"], rss.id):
                     t_rss_item = TRssItem(title=entry["title"], link=entry["link"], published_date=entry["published"],
-                                          rss_id=rss.id, created_date=str(datetime.datetime.today()))
+                                          rss_id=rss.id, created_date=datetime.datetime.today())
                     TRssItemDao.saveTRssItem(t_rss_item)
         catch_autohome()
+        catch_mafengwo()
         print("success")
         time.sleep(60 * 20)
 
