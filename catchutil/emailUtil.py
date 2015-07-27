bf9020036b7a1b1c8867ee60d97acee20fb98076
Lingfei-Zhang <Javagoshell@gmail.com>
2015-07-27 13:15:23 +0800

    add emailutil

--- catchutil/emailUtil.py
+++ catchutil/emailUtil.py
@@ -11,7 +11,10 @@ class EmailUtil(object):
 
     def send_mail(self, msg):
         msg = MIMEText(msg, 'plain', 'utf-8')
-        server = smtplib.SMTP(smtp_server, 25)
-        server.login(email_addr, email_passwd)
-        server.sendmail(email_addr, to_email_addr, msg.as_string())
+        msg['From'] = "sever <%s>" % self.email_addr
+        msg['Subject'] = "server error"
+        server = smtplib.SMTP(self.smtp_server, 25)
+        server.set_debuglevel(1)
+        server.login(self.email_addr, self.email_passwd)
+        server.sendmail(self.email_addr, self.to_email_addr, msg.as_string())
         server.quit()
